59763a84a90cb5ae5a9ed81c939f3a4d1a372fa2
thePoojaVaddadi <newtonnindian95@gmail.com>
2026-05-15 16:26:19 +0530

    fixed flask app import

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,22 +1,15 @@
-from app import app
+from api.app import app
 
 def test_home():
     client = app.test_client()
     res = client.get("/")
     assert res.status_code == 200
 
-def test_health():
-    client = app.test_client()
-    res = client.get("/health")
-    assert res.status_code == 200
-
 def test_predict():
     client = app.test_client()
-
     res = client.post("/predict", json={
         "size": 1200,
         "bedrooms": 2,
         "age": 5
     })
-
     assert res.status_code == 200
